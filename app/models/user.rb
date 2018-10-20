4abf6407229f9a0f1485c62936bfa987f9770be8
Pablo <pablo.um00@gmail.com>
2018-10-20 16:39:55 -0500

    Adds validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,9 @@
 
 class User < ApplicationRecord
   has_secure_password validations: false
+
+  validates :name, presence: true
+  validates :email, uniqueness: true, format: /@/
+  validates :password, presence: true, on: :create
+  validates :password, length: {in: 6..20}, allow_nil: true
 end
